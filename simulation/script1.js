5861a3663390aa968772b644f57ea120f75278e3
vleadadmin <admin@vlabs.ac.in>
2024-09-20 11:29:39 +0000

    https://virtual-labs.github.io/virtual-labs/exp-tensile-strength-dei click on the link to test your code.

---
+++ simulation/script1.js
@@ -0,0 +1,107 @@
+var x = 0;
+var myTimeout;
+var myTimeout1;
+document.getElementById("gif1").style.display="none";
+document.getElementById("gif2").style.display="none";
+document.getElementById("gif3").style.display="none";
+document.getElementById("gif4").style.display="none";
+document.getElementById("gif5").style.display="none";
+document.getElementById("gif6").style.display="none";
+document.getElementById("play1").style.display="none";
+
+
+
+
+document.getElementById("play").onclick=function()
+{
+  
+  if (x == 0)
+    {
+    document.getElementById("png").style.display="none";
+    document.getElementById("gif").style.display="none";
+    document.getElementById("gif1").style.display="inline";
+     myTimeout = setTimeout(myGreeting, 6700);
+   
+  
+  
+    x=1;
+    }
+
+  else if(x == 1)
+   {
+    document.getElementById("gif1").style.display="none";
+    document.getElementById("gif2").style.display="inline";
+    document.getElementById("gif5").style.display="none";
+    myTimeout = setTimeout(myGreeting, 6700);
+
+  
+    x=2;
+   
+
+   }
+
+   else if(x ==2)
+   {
+    document.getElementById("gif2").style.display="none";
+    document.getElementById("gif3").style.display="inline";
+    document.getElementById("play1").style.display="inline";
+    document.getElementById("gif5").style.display="none";
+    myTimeout1 = setTimeout(myGreeting1, 7000);
+
+  
+    x=3;
+   }
+
+   
+  }
+
+  document.getElementById("play1").onclick=function()
+  {
+    document.getElementById("gif3").style.display="none";
+    document.getElementById("gif4").style.display="inline";
+    document.getElementById("gif5").style.display="none";
+    document.getElementById("gif6").style.display="none";
+    myTimeout2 = setTimeout(myGreeting2, 7000);
+  }
+
+
+
+
+  
+  
+    function myGreeting()
+     {
+    document.getElementById("gif5").style.display="inline";
+    myStopFunction();
+  }
+  
+  function myStopFunction() {
+    clearTimeout(myTimeout);
+
+  
+}
+
+function myGreeting1()
+     {
+    document.getElementById("gif6").style.display="inline";
+    myStopFunction1();
+  }
+  
+  function myStopFunction1() {
+    clearTimeout(myTimeout1);
+
+  }
+  function myGreeting2()
+     {
+    document.getElementById("next").style.visibility="visible";
+    myStopFunction2();
+  }
+  
+  function myStopFunction2() {
+    clearTimeout(myTimeout2);
+
+  }
+
+
+
+ 
